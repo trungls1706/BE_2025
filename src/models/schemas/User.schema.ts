7d4808371293d62fae159c7f26fedf720045a82e
trungls1706 <trungls1706@gmail.com>
2025-05-30 16:59:20 +0700

    update

--- src/models/schemas/User.schema.ts
+++ src/models/schemas/User.schema.ts
@@ -45,7 +45,7 @@ export default class User {
 
     this._id = user._id 
     this.name = user.name || ''
-    this.email = user.email
+    this.email = user.email 
     this.date_of_birth = user.date_of_birth || new Date()
     this.password = user.password
     this.created_at = user.created_at || date
@@ -53,11 +53,11 @@ export default class User {
     this.email_verify_token = user.email_verify_token || ''
     this.forgot_password_token = user.forgot_password_token || ''
     this.verify = user.verify || UserVerifyStatus.Unverified
-    this.bio = user.bio
-    this.location = user.location
-    this.website = user.website
-    this.username = user.username
-    this.avatar = user.avatar
-    this.cover_photo = user.cover_photo
+    this.bio = user.bio || ''
+    this.location = user.location || ''
+    this.website = user.website || ''
+    this.username = user.username || ''
+    this.avatar = user.avatar || ''
+    this.cover_photo = user.cover_photo || ''
   }
 }
